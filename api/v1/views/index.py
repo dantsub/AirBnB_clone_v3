20d70b9d8607cb1403804d71f1097790027bf04c
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-04-28 10:18:07 -0500

    Created index of the api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 """ index of the api """
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def status(self):
+    status = {"status": "OK"}
+    return jsonify(status)
