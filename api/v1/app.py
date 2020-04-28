241e74c5818487c6c43f01f7a48da0f8312f450f
dantsub <dany.mrtinez@gmail.com>
2020-04-28 11:33:28 -0500

    task 6 done

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """app Flask api
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -10,6 +10,7 @@ host = getenv('HBNB_API_HOST') or '0.0.0.0'
 port = getenv('HBNB_API_PORT') or 5000
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 
@@ -17,6 +18,12 @@ app.register_blueprint(app_views)
 def close(self):
     """Close session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """ Page not found error 404 """
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == '__main__':
     app.run(host=host, port=port, threaded=True)
